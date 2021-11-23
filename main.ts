317e4976566fe7605b51c058397e92ebe183d89e
chaton99901 <94067162+chaton99901@users.noreply.github.com>
2021-11-23 13:20:11 -0500

    Fichiers initiaux du projet MakeCode

---
+++ main.ts
@@ -0,0 +1,16 @@
+function bleu () {
+	
+}
+function rouge () {
+    pins.analogWritePin(AnalogPin.P1, 1023)
+    pins.analogWritePin(AnalogPin.P2, 1023)
+    pins.analogWritePin(AnalogPin.P8, 1023)
+}
+function vert () {
+    pins.analogWritePin(AnalogPin.P0, 1023)
+    pins.analogWritePin(AnalogPin.P0, 1023)
+    pins.analogWritePin(AnalogPin.P0, 1023)
+}
+basic.forever(function () {
+	
+})
